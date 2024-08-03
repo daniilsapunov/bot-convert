efd63384ee339b8e9f288a9d4b699e89cc17573c
daniil Sapunov <daniilsapunov133@gmail.com>
2024-08-03 23:33:01 +0300

    commit

---
+++ src/query/orm.py
@@ -0,0 +1,10 @@
+# from sqlalchemy import insert
+# from src.database import async_engine, async_session
+# from src.models import User
+#
+#
+# async def save_value():
+#     async with async_session() as session:
+#
+#         await session.commit()
+#
