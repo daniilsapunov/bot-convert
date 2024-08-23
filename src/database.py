6b161b2c48955af7dacbdd1593354539b9a1d8d8
daniil Sapunov <daniilsapunov133@gmail.com>
2024-08-23 11:17:38 +0300

    commit

--- src/database.py
+++ src/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.orm import declarative_base
-from config import settings
+from src.config import settings
 
 Base = declarative_base()
 
