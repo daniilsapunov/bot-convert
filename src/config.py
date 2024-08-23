e3ee17e802a4fea34f15f8c2553715d0fcdf8e3b
daniil Sapunov <daniilsapunov133@gmail.com>
2024-08-23 11:08:06 +0300

    commit

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -8,11 +9,11 @@ load_dotenv()
 class Settings(BaseSettings):
     BOT_TOKEN: str = Field(..., env="BOT_TOKEN")
     OPENAI_API_KEY: str = Field(..., env="OPENAI_API_KEY")
-    PGHOST: str = Field(..., env="PGHOST")
-    PGUSER: str = Field(..., env="PGUSER")
-    PGPASSWORD: str = Field(..., env="PGPASSWORD")
-    PGDATABASE: str = Field(..., env="PGDATABASE")
-    PGPORT: int = Field(..., env="PGPORT")
+    PGHOST: str = Field(..., env="PGHOST", env_default=os.getenv("RAILWAY_POSTGRESQL_HOST"))
+    PGUSER: str = Field(..., env="PGUSER", env_default=os.getenv("RAILWAY_POSTGRESQL_USER"))
+    PGPASSWORD: str = Field(..., env="PGPASSWORD", env_default=os.getenv("RAILWAY_POSTGRESQL_PASSWORD"))
+    PGDATABASE: str = Field(..., env="PGDATABASE", env_default=os.getenv("RAILWAY_POSTGRESQL_DB_NAME"))
+    PGPORT: int = Field(..., env="PGPORT", env_default=os.getenv("RAILWAY_POSTGRESQL_PORT", "5432"))
 
     @property
     def database_url_asyncpg(self):
