d43e34f2ea08078b6ff6a9acf9d2db90b2c1f0f6
daniil Sapunov <daniilsapunov133@gmail.com>
2024-08-23 10:56:57 +0300

    commit

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from pydantic import Field
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
 
 
@@ -9,12 +10,11 @@ class Settings(BaseSettings):
     BOT_TOKEN: str = Field(..., env="BOT_TOKEN")
     OPENAI_API_KEY: str = Field(..., env="OPENAI_API_KEY")
 
-    PGHOST: str = Field(..., env_file="../.env", env="PGHOST", default=os.getenv("RAILWAY_POSTGRESQL_HOST"))
-    PGUSER: str = Field(..., env_file="../.env", env="PGUSER", default=os.getenv("RAILWAY_POSTGRESQL_USER"))
-    PGPASSWORD: str = Field(..., env_file="../.env", env="PGPASSWORD", default=os.getenv("RAILWAY_POSTGRESQL_PASSWORD"))
-    PGDATABASE: str = Field(..., env_file="../.env", env="PGDATABASE", default=os.getenv("RAILWAY_POSTGRESQL_DB_NAME"))
-    PGPORT: int = Field(..., env_file="../.env", env="PGPORT",
-                        default=int(os.getenv("RAILWAY_POSTGRESQL_PORT", "5432")))
+    PGHOST: str = Field(..., env="PGHOST")
+    PGUSER: str = Field(..., env="PGUSER")
+    PGPASSWORD: str = Field(..., env="PGPASSWORD")
+    PGDATABASE: str = Field(..., env="PGDATABASE")
+    PGPORT: int = Field(..., env="PGPORT", )
 
 
 settings = Settings()
